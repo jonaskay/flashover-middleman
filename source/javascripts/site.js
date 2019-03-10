5c6338126b5c33bcc93b67cc2d654a50ad1005a7
Joonas Kykkänen <jonaskay@iki.fi>
2019-03-10 10:26:07 +0200

    Add yearly archives

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,47 @@
 // This is where it all goes :)
+
+document.addEventListener('DOMContentLoaded', function init() {
+  var DISPLAY_BLOCK = 'display: block;';
+  var DISPLAY_NONE = 'display: none;';
+
+  function show(element) {
+    element.setAttribute('style', DISPLAY_BLOCK);
+  }
+
+  function hide(element) {
+    element.setAttribute('style', DISPLAY_NONE);
+  }
+
+  function isHidden(element) {
+    var style = element.getAttribute('style');
+    return style ? style === DISPLAY_NONE : true;
+  }
+
+  function toggle(element) {
+    if (isHidden(element)) {
+      show(element);
+    } else {
+      hide(element);
+    }
+  }
+
+  function clickHandler(element) {
+    var lists = document.querySelectorAll('[data-archive-list]');
+    for (var i = 0; i < lists.length; i++) {
+      var list = lists[i];
+      if (list.dataset.archiveList === element.dataset.archive) {
+        toggle(list);
+      } else {
+        hide(list);
+      }
+    }
+  }
+
+  var links = document.querySelectorAll('[data-archive]');
+  for (var i = 0; i < links.length; i++) {
+    var link = links[i];
+    link.onclick = function handleClick() {
+      return clickHandler(this);
+    };
+  }
+});
